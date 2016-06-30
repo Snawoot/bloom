0acb8943ad218af30d0bf419c5f82863f7799538
Vladislav Yarmak <yarmak.vladislav@gmail.com>
2016-06-30 19:04:17 +0300

    md6

--- bf_hash.c
+++ bf_hash.c
@@ -1,19 +1,23 @@
 #include <stdint.h>
 #include <string.h>
-#include <openssl/sha.h>
+#include "md6.h"
 #include "bf_types.h"
+#include <assert.h>
 
 //Hasher
 uint64_t *bf_hashes(const char* bytes, bf_m_t *Ki, bf_k_t k, bf_hp_t hashpart)
 {
-    unsigned char hashbuf[SHA384_DIGEST_LENGTH];
-    SHA384(bytes,  strlen(bytes), hashbuf);
+    md6_state st;
+
+    assert(md6_init(&st, hashpart * k)==0);
+    assert(md6_update(&st, (unsigned char *)bytes, strlen(bytes) * CHAR_BIT)==0);
+    assert(md6_final(&st, NULL)==0);
 
     int bit, i, j, n=0;
     for (i=0; i < k; i++) {
         size_t curr_key=0;
         for (j=0; j<hashpart; j++,n++) {
-            bit = (hashbuf[n / CHAR_BIT] & ((unsigned char)1 << ((CHAR_BIT - 1) - (n % CHAR_BIT)))) !=0 ? 1 : 0;
+            bit = (st.hashval[n / CHAR_BIT] & ((unsigned char)1 << ((CHAR_BIT - 1) - (n % CHAR_BIT)))) !=0 ? 1 : 0;
             curr_key = (curr_key << 1) | bit;
         }
         Ki[i] = curr_key;
--- bf_hash.h
+++ bf_hash.h
@@ -2,5 +2,5 @@
 #include <openssl/sha.h>
 #include "bf_types.h"
 
-#define BF_HASH_MAX_WIDTH SHA384_DIGEST_LENGTH * 8
+#define BF_HASH_MAX_WIDTH 512
 uint64_t *bf_hashes(const char*, bf_m_t *, bf_k_t, bf_hp_t);
